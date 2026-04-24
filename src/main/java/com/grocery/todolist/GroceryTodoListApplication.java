3ddce8a913e53091dbd409ddf87e1d65043034bb
nicolasholanda <nikolas.nhm14@gmail.com>
2026-04-24 09:15:49 -0300

    Initial commit

---
+++ src/main/java/com/grocery/todolist/GroceryTodoListApplication.java
@@ -0,0 +1,12 @@
+package com.grocery.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryTodoListApplication.class, args);
+    }
+}
